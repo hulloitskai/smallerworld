e7e17c832ea0d1ad8dee4c33096af19f5e96f2a1
kai <hulloitskai@gmail.com>
2025-03-18 10:20:53 -0400

    Fix friend policy

--- app/policies/friend_policy.rb
+++ app/policies/friend_policy.rb
@@ -1,4 +1,10 @@
 # typed: true
 # frozen_string_literal: true
 
-class FriendPolicy < ApplicationPolicy; end
+class FriendPolicy < ApplicationPolicy
+  # == Rules
+  def manage?
+    friend = T.let(record, Friend)
+    friend.user == user
+  end
+end
