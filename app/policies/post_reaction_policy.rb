73c07f47464ae07a8338a311b7884a178cf4a181
kai <hulloitskai@gmail.com>
2025-03-19 01:30:18 -0400

    Fix post reaction permissions

--- app/policies/post_reaction_policy.rb
+++ app/policies/post_reaction_policy.rb
@@ -1,21 +1,10 @@
 # typed: true
 # frozen_string_literal: true
 
-class PostReactionPolicy < ApplicationPolicy
+class PostReactionPolicy < RelaxedPolicy
   # == Rules
   def manage?
     reaction = T.cast(record, PostReaction)
     reaction.friend! == friend!
-  end
-
-  # == Scopes
-  relation_scope do |relation|
-    if (friend = self.friend)
-      relation.joins(:post).where(posts: { author: friend.user! })
-    elsif (user = self.user)
-      relation.joins(:post).where(posts: { author: user })
-    else
-      relation.joins(:post).where(posts: { visibility: "public" })
-    end
   end
 end
