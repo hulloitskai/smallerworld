753257015fe06963238b57f4300ebbba0d4c8cf7
kai <hulloitskai@gmail.com>
2025-03-24 15:38:53 -0400

    Auto-reload CSRF on failure

--- app/helpers/contact.tsx
+++ app/helpers/contact.tsx
@@ -1,7 +1,3 @@
-import { useCallback, useState } from "react";
-
-import { fetchRoute } from "./routes/fetch";
-
 export interface UseContactOptions {
   type?: "email" | "sms";
   subject?: string;
