f6fa69b694c0cd805f60c1c05fd2bf7d9b930969
kai <hulloitskai@gmail.com>
2025-03-31 19:49:49 -0400

    Fix service worker loading in production

--- lib/service_worker_proxy.rb
+++ lib/service_worker_proxy.rb
@@ -17,17 +17,30 @@ class ServiceWorkerProxy
     path = T.let(env.fetch("PATH_INFO"), String)
     query_string = T.let(env.fetch("QUERY_STRING"), String)
     query_values = Addressable::URI.parse("/?" + query_string).query_values
-    env = if path == "/sw" && (worker_value = query_values["worker"])
+    if path == "/sw" && (worker_value = query_values["worker"])
       worker_uri = Addressable::URI.parse(worker_value)
-      env.merge(
-        "PATH_INFO" => worker_uri.path,
-        "REQUEST_PATH" => worker_uri.path,
-        "REQUEST_URI" => worker_value,
-        "QUERY_STRING" => worker_uri.query,
-      )
+      if should_proxy?(env)
+        @proxy.call(env.merge(
+          "PATH_INFO" => worker_uri.path,
+          "REQUEST_PATH" => worker_uri.path,
+          "REQUEST_URI" => worker_value,
+          "QUERY_STRING" => worker_uri.query,
+        ))
+      else
+        files = Rack::Files.new(Rails.public_path.to_s)
+        files.call(env.merge(
+          "PATH_INFO" => worker_uri.path,
+          "HTTP_ACCEPT" => "application/javascript",
+        ))
+      end
     else
-      env
+      @proxy.call(env)
     end
-    @proxy.call(env)
+  end
+
+  sig { params(env: T::Hash[String, T.untyped]).returns(T::Boolean) }
+  def should_proxy?(env)
+    !!@proxy.send(:vite_should_handle?, env) &&
+      @proxy.send(:dev_server_running?)
   end
 end
