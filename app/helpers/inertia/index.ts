9202c4de110051003a4d93829a54243b5870e6a4
kai <hulloitskai@gmail.com>
2025-04-15 17:52:43 -0400

    Handle 5XX errors when performing an Inertia fetch

--- app/helpers/inertia/index.ts
+++ app/helpers/inertia/index.ts
@@ -1,33 +1,4 @@
-import { AxiosHeaders } from "axios";
-
 export type { EmailComponent } from "./email";
 export type { PageComponent } from "./page";
 export { PageType, parsePageImports, resolvePageType } from "./page";
 export { setupInertia } from "./setup";
-
-export const handleNonInertiaDocumentResponse = (): VoidFunction => {
-  return router.on("invalid", event => {
-    const { status, headers, data, request } = event.detail.response;
-    if (
-      status >= 200 &&
-      status < 300 &&
-      headers instanceof AxiosHeaders &&
-      request instanceof XMLHttpRequest
-    ) {
-      const contentType = headers.get("Content-Type");
-      const url = new URL(request.responseURL);
-      if (
-        url.host === location.host &&
-        typeof contentType === "string" &&
-        contentType.startsWith("text/html") &&
-        typeof data === "string"
-      ) {
-        event.preventDefault();
-        document.open();
-        document.write(data);
-        document.close();
-        history.pushState(null, "", url.toString());
-      }
-    }
-  });
-};
--- app/helpers/inertia/setup.ts
+++ app/helpers/inertia/setup.ts
@@ -1,5 +1,6 @@
 import { router } from "@inertiajs/core";
 import { closeAllModals } from "@mantine/modals";
+import { AxiosHeaders } from "axios";
 
 export const setupInertia = (): void => {
   router.on("before", ({ detail: { visit } }) => {
@@ -9,7 +10,53 @@ export const setupInertia = (): void => {
     }
   });
   router.on("invalid", event => {
-    console.warn("invalid Inertia response", event.detail.response.data);
+    console.warn("Invalid Inertia response", event.detail.response.data);
+    const { status, headers, data, request } = event.detail.response;
+    if (
+      !(headers instanceof AxiosHeaders) ||
+      !(request instanceof XMLHttpRequest)
+    ) {
+      return;
+    }
+    if (status >= 200 && status < 300) {
+      const contentType = headers.get("Content-Type");
+      const url = new URL(request.responseURL);
+      if (
+        url.host === location.host &&
+        typeof contentType === "string" &&
+        contentType.startsWith("text/html") &&
+        typeof data === "string"
+      ) {
+        event.preventDefault();
+        document.open();
+        document.write(data);
+        document.close();
+        history.pushState(null, "", url.toString());
+      }
+    } else if (status >= 500 && status < 600) {
+      const contentType = headers.get("Content-Type");
+      event.preventDefault();
+      let description: string | undefined = undefined;
+      if (
+        typeof contentType === "string" &&
+        contentType.startsWith("text/plain") &&
+        typeof data === "string"
+      ) {
+        description = data;
+      } else if (
+        typeof contentType === "string" &&
+        contentType.startsWith("application/json") &&
+        typeof data === "object"
+      ) {
+        if ("error" in data) {
+          const { error } = data;
+          if (typeof error === "string") {
+            description = error;
+          }
+        }
+      }
+      toast.error("Network request failed", { description });
+    }
   });
   router.on("exception", event => {
     console.error(
