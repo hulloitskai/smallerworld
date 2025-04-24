54618b29922e55babfcdac0a9e72e33c987f8b30
kai <hulloitskai@gmail.com>
2025-04-24 00:29:31 -0400

    Use subdomains in manifests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,7 @@ class UsersController < ApplicationController
       else
         user_manifest_icons(user)
       end
+    subdomain_options = Rails.env.production? ? { subdomain: user.handle } : {}
     render(
       json: {
         name: "#{user.name}'s world",
@@ -57,7 +58,11 @@ class UsersController < ApplicationController
         description: "life updates, personal invitations, poems, and more!",
         icons:,
         display: "standalone",
-        start_url: user_path(user, friend_token: current_friend.access_token),
+        start_url: user_url(
+          user,
+          friend_token: current_friend.access_token,
+          **subdomain_options,
+        ),
       },
       content_type: "application/manifest+json",
     )
