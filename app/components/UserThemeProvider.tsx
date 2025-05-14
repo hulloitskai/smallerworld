e47ee11c2ae0d27bf61fb136f12f30e5211a25d1
kai <hulloitskai@gmail.com>
2025-05-14 17:06:35 -0400

    Fix color scheme bug on Android

--- app/components/UserThemeProvider.tsx
+++ app/components/UserThemeProvider.tsx
@@ -1,3 +1,5 @@
+import { useComputedColorScheme } from "@mantine/core";
+
 import {
   DARK_USER_THEMES,
   IMAGE_USER_THEMES,
@@ -17,30 +19,45 @@ const UserThemeProvider: FC<UserThemeProviderProps> = ({ children }) => {
   const [theme, setTheme] = useState<UserTheme | null>(null);
 
   // == Apply theme on document body
+  const computedColorScheme = useComputedColorScheme();
   useEffect(() => {
-    if (theme) {
-      document.documentElement.setAttribute("data-user-theme", theme);
-      document.body.style.setProperty(
-        "--mantine-color-body",
-        USER_THEME_BACKGROUND_COLORS[theme],
-      );
-      return () => {
-        document.documentElement.removeAttribute("data-user-theme");
-        document.body.style.removeProperty("--mantine-color-body");
-      };
+    if (!theme) {
+      return;
     }
-  }, [theme]);
+    document.documentElement.setAttribute("data-user-theme", theme);
+    document.body.style.setProperty(
+      "--mantine-color-body",
+      USER_THEME_BACKGROUND_COLORS[theme],
+    );
+    const themeColorScheme = DARK_USER_THEMES.includes(theme)
+      ? "dark"
+      : "light";
+    document.documentElement.setAttribute(
+      "data-mantine-color-scheme",
+      themeColorScheme,
+    );
+    return () => {
+      document.documentElement.removeAttribute("data-user-theme");
+      document.body.style.removeProperty("--mantine-color-body");
+      document.documentElement.setAttribute(
+        "data-mantine-color-scheme",
+        computedColorScheme,
+      );
+    };
+  }, [theme]); // eslint-disable-line react-hooks/exhaustive-deps
 
   // == Set color scheme
-  const { setColorScheme, clearColorScheme } = useMantineColorScheme();
-  useEffect(() => {
-    if (theme) {
-      const isDark = DARK_USER_THEMES.includes(theme);
-      setColorScheme(isDark ? "dark" : "light");
-    } else {
-      clearColorScheme();
-    }
-  }, [theme]); // eslint-disable-line react-hooks/exhaustive-deps
+  // const { setColorScheme, clearColorScheme } = useMantineColorScheme();
+  // useEffect(() => {
+  //   if (!theme) {
+  //     return;
+  //   }
+  //   const isDark = DARK_USER_THEMES.includes(theme);
+  //   setColorScheme(isDark ? "dark" : "light");
+  //   return () => {
+  //     clearColorScheme();
+  //   };
+  // }, [theme]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <UserThemeContext.Provider
