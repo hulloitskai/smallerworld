88df45ae586d93fa2f7e2e34a57967db12eb00f2
kai <hulloitskai@gmail.com>
2025-06-08 17:16:40 -0400

    Don't oversize small images on ImageStack

--- app/helpers/images.ts
+++ app/helpers/images.ts
@@ -11,13 +11,16 @@ export const clampedImageDimensions = (
   const { width, height } = dimensions;
   const aspectRatio = width / height;
   const maxAspectRatio = maxWidth / maxHeight;
-  return aspectRatio > maxAspectRatio
-    ? {
-        width: maxWidth,
-        height: maxWidth / aspectRatio,
-      }
-    : {
-        width: maxHeight * aspectRatio,
-        height: maxHeight,
-      };
+  if (aspectRatio > maxAspectRatio) {
+    const actualWidth = Math.min(maxWidth, width);
+    return {
+      width: actualWidth,
+      height: actualWidth / aspectRatio,
+    };
+  }
+  const actualHeight = Math.min(maxHeight, height);
+  return {
+    width: actualHeight * aspectRatio,
+    height: actualHeight,
+  };
 };
