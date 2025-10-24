825ca9a34c7004d6d1b3221be1abd31ac2a73ba4
kai <hulloitskai@gmail.com>
2025-10-23 20:10:07 -0400

    Fix CI build issues

--- app/components/WorldFooter.tsx
+++ app/components/WorldFooter.tsx
@@ -2,9 +2,12 @@ import { type InertiaLinkProps } from "@inertiajs/react";
 import {
   AppShell,
   type AppShellFooterProps,
+  Image,
   NavLink,
   type NavLinkProps,
 } from "@mantine/core";
+
+import logoSrc from "~/assets/images/logo.png";
 
 import classes from "./WorldFooter.module.css";
 
@@ -25,12 +28,15 @@ const WorldFooter = forwardRef<HTMLDivElement, WorldFooterProps>(
         <NavLinkItem
           href={withTrailingSlash(routes.world.show.path())}
           label="your world"
+          leftSection={<Image src={logoSrc} h={18} />}
           active={!isLocalUniverse}
         />
         <NavLinkItem
           href={routes.localUniverse.show.path()}
           label="local universe"
+          leftSection="💫"
           active={isLocalUniverse}
+          className={classes.localUniverseNavLink}
         />
       </AppShell.Footer>
     );
@@ -42,6 +48,10 @@ export default WorldFooter;
 interface NavLinkItemProps
   extends NavLinkProps,
     Omit<InertiaLinkProps, "color" | "label" | "onChange" | "style"> {}
-const NavLinkItem: FC<NavLinkItemProps> = props => (
-  <NavLink component={Link} variant="subtle" {...props} />
+const NavLinkItem: FC<NavLinkItemProps> = ({ className, ...otherProps }) => (
+  <NavLink
+    className={cn(classes.navLink, className)}
+    component={Link}
+    {...otherProps}
+  />
 );
