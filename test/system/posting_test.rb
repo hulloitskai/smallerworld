535cb32587c9ff4b5cff535cb8013d98377046c0
kai <hulloitskai@gmail.com>
2025-12-08 19:56:40 -0500

    Added test for posting in a space

--- test/system/posting_test.rb
+++ test/system/posting_test.rb
@@ -35,4 +35,36 @@ class PostingTest < ApplicationSystemTestCase
     # Assert test post is visible
     assert_text "this is a test journal entry"
   end
+
+  test "can post a journal entry in a space" do
+    # Sign in as Testy
+    testy = users(:testy)
+    visit new_session_path
+
+    phone = testy.phone
+    fill_in "country_code", with: phone.country_code
+    fill_in "national_phone_number", with: phone.national(false)
+    click_link_or_button "send login code"
+    click_link_or_button "auto-fill code", wait: 4.seconds
+    click_link_or_button "sign in", wait: 4.seconds
+
+    # Navigate to space page
+    space = spaces(:testy_space)
+    visit space_path(space)
+
+    # Click 'new post' button
+    click_link_or_button "new post"
+
+    # Click 'new journal entry' button
+    click_link_or_button "new journal entry"
+
+    # Fill in the contenteditable with test content
+    find("[contenteditable='true']").set("this is a test space journal entry")
+
+    # Click 'post' button
+    click_link_or_button "post"
+
+    # Assert test post is visible
+    assert_text "this is a test space journal entry"
+  end
 end
